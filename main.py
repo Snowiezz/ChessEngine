3774d919a8107dea738dfd20b2bdfd75f1d8bd4b
Snowiezz <121179260+Snowiezz@users.noreply.github.com>
2026-08-11 03:28:49 +0100

    finish square to index

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ pieces = {
     "p":["Pawn"],
     ".":["Empty"]
 }
+
+# get pieces
 def get_piece_colour(piece):
     if piece == ".":
         return None
@@ -29,9 +31,20 @@ def get_piece_type(piece):
     if piece == ".":
         return None
     return pieces[piece.lower()][0]
-
-
 def get_piece(position):
     return get_piece_colour(board[position]),get_piece_type(board[position])
 
-print(get_piece(62))
+def square_to_index(square):
+    if len(square) != 2:
+        #improve later
+        return False
+    row = square[0]
+    column = square[1]
+    if row not in "abcdefgh" or column not in "12345678":
+        return False
+
+
+
+
+print(square_to_index("f2"))
+
